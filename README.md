ed7c7d58f3c05abf9b39d10d21294ab44149457f
bonohub13 <ken.saito.0813@gmail.com>
2020-10-16 08:40:32 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A very simple countup and countdown timer
---
+++ scripts/timer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+import time
+from termcolor import colored
+
+class CountdownTimer:
+    def __init__(self):
+        self.init_time = int(input("input time to countdown [minutes]: "))
+        self.init_time = self.init_time*60
+        self.counter = 0
+
+    def run(self):
+        while self.counter < self.init_time:
+            time.sleep(1)
+            self.counter += 1
+            print("time left: [ {}:{} ]\r".format((self.init_time-self.counter)//60, (self.init_time-self.counter)%60), end="")
+
+class CountupTimer:
+    def __init__(self):
+        self.counter = 0
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            self.counter += 1
+            seconds_tmp = self.counter%60
+            seconds = seconds_tmp if seconds_tmp > 9 else "0{}".format(seconds_tmp)
+            minutes_tmp = self.counter//60
+            minutes = minutes_tmp if minutes_tmp > 9 else "0{}".format(minutes_tmp)
+            font_color = "white" if minutes_tmp < 5 else "red"
+            self.time = "[ {}:{} ]\r".format(minutes, seconds)
+            print(colored("elapsed time: {}\r".format(self.time), font_color), end="")
+
+if __name__ == "__main__":
+    timer = CountupTimer()
+    try:
+        timer.run()
+    except KeyboardInterrupt:
+        print("\r", end="")
+        print("Timer halted at " + timer.time)
